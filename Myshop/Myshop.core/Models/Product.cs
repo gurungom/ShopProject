d81297380a9fdf8eb53d498945236de5a859418e
gurungom <41574397+gurungom@users.noreply.github.com>
2020-01-25 23:48:22 +0545

    Models and Storage Setup

---
+++ Myshop/Myshop.core/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myshop.core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        [StringLength(10)]
+        
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+    }
+}
